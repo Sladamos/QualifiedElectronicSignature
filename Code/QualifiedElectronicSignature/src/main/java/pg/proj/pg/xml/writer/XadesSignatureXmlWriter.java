3113f4fd961b3f12126af314fc42d59ec2e35718
Sladamos <sladamo@wp.pl>
2024-04-08 21:30:39 +0200

    Java-FX warnings removed

--- Code/QualifiedElectronicSignature/src/main/java/pg/proj/pg/xml/writer/XadesSignatureXmlWriter.java
+++ Code/QualifiedElectronicSignature/src/main/java/pg/proj/pg/xml/writer/XadesSignatureXmlWriter.java
@@ -2,28 +2,26 @@ package pg.proj.pg.xml.writer;
 
 import pg.proj.pg.xml.info.SignatureXmlInfo;
 
-import java.util.Arrays;
 import java.util.Base64;
 
 public class XadesSignatureXmlWriter implements SignatureXmlWriter {
     @Override
-    public String toXml(SignatureXmlInfo xmlInfo) {;
-        StringBuilder xmlBuilder = new StringBuilder();
-        xmlBuilder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-        xmlBuilder.append("<document>\n");
-        xmlBuilder.append("\t<details>\n");
-        xmlBuilder.append("\t\t<lastModificationDate>").append(xmlInfo.sourceInfo().documentDetails().modificationDate()).append("</lastModificationDate>\n");
-        xmlBuilder.append("\t\t<creationDate>").append(xmlInfo.sourceInfo().documentDetails().creationDate()).append("</creationDate>\n");
-        xmlBuilder.append("\t\t<fileName>").append(xmlInfo.sourceInfo().documentDetails().fileInfo().fileName().toLowerCase().split("\\.")[0]).append("</fileName>\n");
-        xmlBuilder.append("\t\t<fileExtension>").append(xmlInfo.sourceInfo().documentDetails().fileInfo().extension().strValue()).append("</fileExtension>\n");
-        xmlBuilder.append("\t\t<length>").append(xmlInfo.sourceInfo().documentDetails().size()).append("</length>\n");
-        xmlBuilder.append("\t</details>\n");
-        xmlBuilder.append("\t<content>\n");
-        xmlBuilder.append("\t\t<author>").append(xmlInfo.sourceInfo().authorInfo().name()).append("</author>\n");
-        xmlBuilder.append("\t\t<signatureDate>").append(xmlInfo.sourceInfo().documentDateTime()).append("</signatureDate>\n");
-        xmlBuilder.append("\t\t<signature>").append(Base64.getEncoder().encodeToString(xmlInfo.signedValue())).append("</signature>\n");
-        xmlBuilder.append("\t</content>\n");
-        xmlBuilder.append("</document>");
-        return xmlBuilder.toString();
+    public String toXml(SignatureXmlInfo xmlInfo) {
+        String xmlBuilder = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<document>\n" +
+                "\t<details>\n" +
+                "\t\t<lastModificationDate>" + xmlInfo.sourceInfo().documentDetails().modificationDate() + "</lastModificationDate>\n" +
+                "\t\t<creationDate>" + xmlInfo.sourceInfo().documentDetails().creationDate() + "</creationDate>\n" +
+                "\t\t<fileName>" + xmlInfo.sourceInfo().documentDetails().fileInfo().fileName().toLowerCase().split("\\.")[0] + "</fileName>\n" +
+                "\t\t<fileExtension>" + xmlInfo.sourceInfo().documentDetails().fileInfo().extension().strValue() + "</fileExtension>\n" +
+                "\t\t<length>" + xmlInfo.sourceInfo().documentDetails().size() + "</length>\n" +
+                "\t</details>\n" +
+                "\t<content>\n" +
+                "\t\t<author>" + xmlInfo.sourceInfo().authorInfo().name() + "</author>\n" +
+                "\t\t<signatureDate>" + xmlInfo.sourceInfo().documentDateTime() + "</signatureDate>\n" +
+                "\t\t<signature>" + Base64.getEncoder().encodeToString(xmlInfo.signedValue()) + "</signature>\n" +
+                "\t</content>\n" +
+                "</document>";
+        return xmlBuilder;
     }
 }
